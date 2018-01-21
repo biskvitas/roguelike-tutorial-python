ce29cab92eee2058b8c1c99bccf629e8cc469df9
Vitalij Spakov <bigvit2@gmail.com>
2018-01-21 22:52:36 +0000

    Tutorial Part 5: entity collision

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,6 +1,8 @@
+import libtcodpy as libtcod
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from random import randint
+from entity import Entity
 
 class GameMap:
     def __init__(self, width, height):
@@ -23,7 +25,7 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room):
         rooms = []
         num_rooms = 0
 
@@ -72,7 +74,9 @@ class GameMap:
                         self.create_v_tunnel(prev_y, new_y, prev_x)
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
-                        # finally, append the new room to the list
+                self.place_entities(new_room, entities, max_monsters_per_room)
+    
+                # finally, append the new room to the list
                 rooms.append(new_room)
                 num_rooms += 1
 
@@ -88,3 +92,20 @@ class GameMap:
             return True
 
         return False
+
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                else:
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+
+                entities.append(monster)
